924358b574956828a4db054459ad2d0e40770b7c
Connie Skomra <connie@cskomra.com>
2017-11-15 16:32:58 -0500

    feat add ability to add Whipped Cream or Chocolate feat limit order to between 1 to 100 cups of coffee

--- app/src/main/java/com/cskomra/justjava/MainActivity.java
+++ app/src/main/java/com/cskomra/justjava/MainActivity.java
@@ -1,17 +1,26 @@
 package com.cskomra.justjava;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
     int pricePerCup = 5;
+    int priceForWhip = 1;
+    int priceForChocolate = 2;
+    boolean hasWhippedCream = false;
+    boolean hasChocolate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +41,29 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view){
-        quantity = quantity + 1;
-        displayQuantity(quantity);
+        if (quantity == 100){
+            Context context = getApplicationContext();
+            CharSequence text = "Maximum coffee order is 100.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }else {
+            quantity = quantity + 1;
+            displayQuantity(quantity);
+        }
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view){
-        if (quantity > 0){
+        if (quantity == 1) {
+            Context context = getApplicationContext();
+            CharSequence text = "Minimum coffee order is 1.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }else{
             quantity = quantity - 1;
             displayQuantity(quantity);
         }
@@ -50,8 +73,19 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+
+        //get checked state from Whipped Cream CheckBox view
+        CheckBox addWhipCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        hasWhippedCream = addWhipCheckBox.isChecked();
+
+        CheckBox addChocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        hasChocolate = addChocolateCheckBox.isChecked();
+
+        EditText nameView = (EditText) findViewById(R.id.name_text_input);
+        String name = nameView.getText().toString();
+
         int price = calculatePrice();
-        String message = createOrderSummary(price);
+        String message = createOrderSummary(price, name);
         displayMessage(message);
     }
 
@@ -69,14 +103,21 @@ public class MainActivity extends AppCompatActivity {
      *
      */
     private int calculatePrice() {
-        return quantity * pricePerCup;
+        int whip = hasWhippedCream ? priceForWhip : 0;
+        int choc = hasChocolate ? priceForChocolate : 0;
+        int basePrice = pricePerCup + whip + choc;
+
+        return quantity * basePrice;
     }
 
     /**
      * Creates a message for the order
      */
-    private String createOrderSummary(int price){
-        return  "Name: Connie Skomra" + "\n" +
+    private String createOrderSummary(int price,
+                                      String name){
+        return  "Name: " + name + "\n" +
+                "Add whipped cream? " + hasWhippedCream + "\n" +
+                "Add chocolate? " + hasChocolate + "\n" +
                 "Quantity: " + quantity + "\n" +
                 "Total: $" + price + "\n" +
                 "Thank you!";
